9f88d87765734c4fc9a030d5b7a762f56577cd10
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-11-12 19:56:15 +0100

    moved import

--- mpl_bsic/__init__.py
+++ mpl_bsic/__init__.py
@@ -46,8 +46,8 @@ Module Components
 """
 
 from .apply_bsic_style import apply_bsic_style  # noqa
+from .apply_bsic_logo import apply_bsic_logo  # noqa
 from .check_figsize import check_figsize  # noqa
 from .plot_trade import plot_trade  # noqa
 from .format_timeseries_axis import format_timeseries_axis  # noqa
 from .preprocess_dataframe import preprocess_dataframe  # noqa
-from .apply_bsic_logo import apply_bsic_logo  # noqa
