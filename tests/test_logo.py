a6f0bd78e13efa418c4d38f1ba719d1cf6def413
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2023-11-29 01:17:55 +0100

    feat: testing logo (first version)

--- tests/test_logo.py
+++ tests/test_logo.py
@@ -1,26 +1,116 @@
-from matplotlib.axes import Axes
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes import Axes
 from tools import image_compare
 
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../")
-from mpl_bsic import apply_bsic_style, apply_bsic_logo
+from mpl_bsic import apply_bsic_logo, apply_bsic_style
 
 
-x = np.linspace(0, 5, 100)
-y1 = np.sin(x)
-y2 = np.cos(x)
+def _gen_data():
+    x = np.linspace(0, 5, 100)
+    y = np.sin(x)
 
-fig, ax = plt.subplots(1, 1)
-apply_bsic_logo(fig, ax)
+    return x, y
 
-ax.plot(x, y1, label="sin(x)")
-ax.plot(x, y2, label="cos(x)")
-ax.set_title("BSIC Styling Library")
-ax.legend()
-apply_bsic_style(fig, ax)
-fig.savefig("test_logo.svg", dpi=1200, bbox_inches="tight")
-plt.show()
+
+class TestLogoTopLeft:
+    """Tests when the logo is positioned on the top left corner of the plot"""
+
+    @image_compare(baseline_images="test_logo_top_lx")
+    def test_logo_1(self):
+        """Tests when the function is called immediately"""
+        x, y = _gen_data()
+
+        fig, ax = plt.subplots(1, 1)
+        ax: Axes
+        apply_bsic_style(fig, ax)
+        apply_bsic_logo(fig, ax)
+
+        ax.plot(x, y)
+        ax.set_title("Sin(x)")
+        # plt.show()
+
+        # fig.savefig("test_logo_1.png")
+
+        # plt.show()
+
+        return fig
+
+    @image_compare(baseline_images="test_logo_top_lx")
+    def test_logo_2(self):
+        """Tests when the function gets in the middle"""
+
+        x, y = _gen_data()
+        fig, ax = plt.subplots(1, 1)
+        ax: Axes
+
+        ax.plot(x, y)
+        apply_bsic_style(fig, ax)
+        apply_bsic_logo(fig, ax)
+        ax.set_title("Sin(x)")
+
+        return fig
+
+    @image_compare(baseline_images="test_logo_top_lx")
+    def test_logo_3(self):
+        """Tests when the function gets called at the very end"""
+
+        x, y = _gen_data()
+        fig, ax = plt.subplots(1, 1)
+        ax: Axes
+
+        ax.plot(x, y)
+        ax.set_title("Sin(x)")
+        apply_bsic_style(fig, ax)
+        apply_bsic_logo(fig, ax)
+
+        return fig
+
+
+class TestLogoTopRight:
+    """Tests when the logo is positioned on the top left corner of the plot"""
+
+    @image_compare(baseline_images="test_logo_top_rx")
+    def test_logo_1(self):
+        """Tests when the function is called immediately"""
+        x, y = _gen_data()
+
+        fig, ax = plt.subplots(1, 1)
+        ax: Axes
+        apply_bsic_style(fig, ax)
+        apply_bsic_logo(fig, ax, location="top right")
+
+        ax.plot(x, y)
+        ax.set_title("Sin(x)")
+
+        return fig
+
+    @image_compare(baseline_images="test_logo_top_rx")
+    def test_logo_2(self):
+        """Tests when the function gets in the middle"""
+
+        x, y = _gen_data()
+        fig, ax = plt.subplots(1, 1)
+        ax: Axes
+
+        ax.plot(x, y)
+        apply_bsic_style(fig, ax)
+        apply_bsic_logo(fig, ax, location="top right")
+        ax.set_title("Sin(x)")
+
+        return fig
+
+    @image_compare(baseline_images="test_logo_top_rx")
+    def test_logo_3(self):
+        """Tests when the function gets called at the very end"""
+
+        x, y = _gen_data()
+        fig, ax = plt.subplots(1, 1)
+        ax: Axes
+
+        ax.plot(x, y)
+        ax.set_title("Sin(x)")
+        apply_bsic_style(fig, ax)
+        apply_bsic_logo(fig, ax, location="top right")
+
+        return fig
