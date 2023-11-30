0dee01dae815959b1b0e90c8b916cf3d9aab9d7f
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2023-11-30 17:00:22 +0100

    refactor: reorganize imports

--- mpl_bsic/__init__.py
+++ mpl_bsic/__init__.py
@@ -1,8 +1,8 @@
-from .apply_bsic_style import apply_bsic_style  # noqa
 from .apply_bsic_logo import apply_bsic_logo  # noqa
+from .apply_bsic_style import apply_bsic_style  # noqa
 from .check_figsize import check_figsize  # noqa
-from .plot_trade import plot_trade  # noqa
 from .format_timeseries_axis import format_timeseries_axis  # noqa
+from .plot_trade import plot_trade  # noqa
 from .preprocess_dataframe import preprocess_dataframe  # noqa
 
 __version__ = "1.1.5"
