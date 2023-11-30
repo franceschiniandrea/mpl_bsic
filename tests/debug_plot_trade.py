fee2b180983c98402b3f9dabfc9cc20a7f9091ae
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2023-11-30 17:04:01 +0100

    feat: debug plot_trade

---
+++ tests/debug_plot_trade.py
@@ -0,0 +1,14 @@
+import conftest  # noqa
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from mpl_bsic import plot_trade
+
+trade = pd.read_excel("tests/data/trade.xlsx", index_col=0)
+trade.sort_index(inplace=True)
+
+plot_trade(
+    trade["Underlying"], trade["PnL"], "nominal", "Swap Spreads Trade", "Swap Spread"
+)
+
+plt.show()
