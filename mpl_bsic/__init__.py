e259cf53654f4037e35d4350e869ea1f055d8601
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2024-02-24 19:20:36 +0100

    refactor(__init__): add export_figure to default exports

--- mpl_bsic/__init__.py
+++ mpl_bsic/__init__.py
@@ -1,6 +1,7 @@
 from .apply_bsic_logo import apply_bsic_logo  # noqa
 from .apply_bsic_style import apply_bsic_style  # noqa
 from .check_figsize import check_figsize  # noqa
+from .export_figure import export_figure  # noqa
 from .format_timeseries_axis import format_timeseries_axis  # noqa
 from .plot_trade import plot_trade  # noqa
 from .preprocess_dataframe import preprocess_dataframe  # noqa
